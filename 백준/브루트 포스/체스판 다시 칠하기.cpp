dbd9bb3d25c0ffcdc837b71d323538d75f71eb2d
Heojunhwan <gjwnsghks02@gmail.com>
2020-03-27 22:54:18 +0900

    200327

--- 백준/브루트 포스/체스판 다시 칠하기.cpp
+++ 백준/브루트 포스/체스판 다시 칠하기.cpp
@@ -6,24 +6,29 @@ string chess[8]={"WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWBW","WBWBWBWB","BWBWBWB
 int main(){
 	int n,m;
 	cin>>n>>m;
+	
 	for(int i=0;i<n;i++){
-		cin>>in[n];
+		cin>>in[i];
 	}
+	//cout<<"d";
 	int ans=64;
-	for(int i=0;i<n-8;i++){
-		for(int j=0;j<n-8;j++){
+	for(int i=0;i<=n-8;i++){
+		for(int j=0;j<=m-8;j++){
 			int cnt=0;
+	//		cout<<i<<" "<<j;
 			for(int k=i;k<i+8;k++){
 				for(int l=j;l<j+8;l++){
-					cout<<in[k][l]<<" "<<chess[k-i][l-j];
+					//cout<<in[k][l]<<" "<<chess[k-i][l-j]<<"\n";
 					if(in[k][l]!=chess[k-i][l-j])
 						cnt++;
 				}
 			}
+			//cout<<i<<" "<<j<<" "<<cnt<<"\n";
 			cnt=min(cnt,64-cnt);
 			ans=min(ans,cnt);
 		}
-	}		
+	}
+			
 	cout<<ans;
 }
 
